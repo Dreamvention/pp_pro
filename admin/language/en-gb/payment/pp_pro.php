b9de0aa3631b286303728f97032dc1a8d3395b25
ПК\Беликов Артем <dev.dreamvention@gmail.com>
2019-05-08 13:00:56 +0300

    установка

--- admin/language/en-gb/payment/pp_pro.php
+++ admin/language/en-gb/payment/pp_pro.php
@@ -1,35 +1,34 @@
 <?php
 // Heading
-$_['heading_title']		 				= 'PayPal Pro';
+$_['heading_title']						= 'PayPal Pro';
 
 // Text
-$_['text_pp_pro']		 				= '<a target="_BLANK" href="https://www.paypal.com/uk/mrb/pal=V4T754QB63XXL"><img src="view/image/payment/paypal.png" alt="PayPal Website Payment Pro" title="PayPal Website Payment Pro iFrame" style="border: 1px solid #EEEEEE;" /></a>';
-$_['text_extension']     				= 'Extensions';
-$_['text_edit']          				= 'Edit PayPal Pro';
-$_['text_authorization'] 				= 'Authorization';
-$_['text_sale']			 				= 'Sale';
+$_['text_pp_pro']						= '<a target="_BLANK" href="https://www.paypal.com/uk/mrb/pal=V4T754QB63XXL"><img src="view/image/payment/paypal.png" alt="PayPal Website Payment Pro" title="PayPal Website Payment Pro iFrame" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_payment']					 	= 'Payment';
+$_['text_edit']                     	= 'Edit PayPal Pro';
+$_['text_authorization']				= 'Authorization';
+$_['text_sale']							= 'Sale';
 $_['text_cardinal']						= 'Paypal Pro requires 3D Secure payment, please create a <a href="https://paypal3dsregistration.cardinalcommerce.com/UI/registrationcontactpage.aspx" target="_blank">Cardinal account</a> to retrieve the API credentials.';
 
 // Entry
-$_['entry_username']	 				= 'API Username';
-$_['entry_password']	 				= 'API Password';
-$_['entry_signature']	 				= 'API Signature';
-$_['entry_test']		 				= 'Test Mode';
-$_['entry_transaction']	 				= 'Transaction Method';
+$_['entry_username']					= 'API Username';
+$_['entry_password']					= 'API Password';
+$_['entry_signature']					= 'API Signature';
+$_['entry_test']						= 'Test Mode';
+$_['entry_transaction']					= 'Transaction Method';
 $_['entry_cardinal_status']             = 'Cardinal Status';
 $_['entry_cardinal_api_id']             = 'Cardinal API ID';
 $_['entry_cardinal_api_key']            = 'Cardinal API Key';
 $_['entry_cardinal_org_unit_id']    	= 'Cardinal Org Unit ID';
-$_['entry_total']		 				= 'Total';
-$_['entry_order_status'] 				= 'Order Status';
-$_['entry_geo_zone']	 				= 'Geo Zone';
-$_['entry_status']		 				= 'Status';
-$_['entry_sort_order']	 				= 'Sort Order';
-
+$_['entry_total']						= 'Total';
+$_['entry_order_status']				= 'Order Status';
+$_['entry_geo_zone']					= 'Geo Zone';
+$_['entry_status']						= 'Status';
+$_['entry_sort_order']					= 'Sort Order';
 
 // Help
-$_['help_test']			 				= 'Use the live or testing (sandbox) gateway server to process transactions?';
-$_['help_total']		 				= 'The checkout total the order must reach before this payment method becomes active';
+$_['help_test']							= 'Use the live or testing (sandbox) gateway server to process transactions?';
+$_['help_total']						= 'The checkout total the order must reach before this payment method becomes active';
 
 // Success
 $_['success_save']		 				= 'Success: You have modified PayPal Website Payment Pro Checkout account details!';
